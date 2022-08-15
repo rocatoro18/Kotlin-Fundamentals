f640e9df34aafa54e88f3c21704457681671e24f
rocatoro18 <rocatoro18@outlook.com>
2022-08-14 18:57:28 -0700

    More fundamentals

--- app/src/main/java/rocatoro/kotlin_fundamentals/Basics.kt
+++ app/src/main/java/rocatoro/kotlin_fundamentals/Basics.kt
@@ -1,5 +1,7 @@
 package rocatoro.kotlin_fundamentals
 
+import kotlin.math.min
+
 fun main(){
     // var can be overriden
     // val can not be overrriden, can only be set once (cannot be reassigned)
@@ -46,5 +48,168 @@ fun main(){
 
     println("Last character $lastCharacter")
 
+    // Aritmetic operators (+,-,*,/,%)
+    var result = 10 + 8
+    //result /= 2
+    //result *= 2
+    //result -= 2
+    //result %= 2
+    var a = 8.0
+    var b = 2
+    var resultDouble: Double
+    resultDouble = a / b
 
+    println(resultDouble)
+
+    // Comparison operators (==, < ,> , <=, >=, !=)
+
+    val isEqual = 5 == 3
+    val isNotEqual = 5 != 5
+
+    println("Is equal $isNotEqual")
+    // String interpolation
+    println("Is 10 greater than 5 ${5>3}")
+
+    // Assignment operator (+=, -=, *=, /=, %=)
+    var myNum = 5
+    myNum += 5
+    println("My num is $myNum")
+    myNum -= 4
+    println("My num is $myNum")
+    // Increment & decrement operators (++, --)
+    myNum++
+    println("My num is $myNum")
+    // Inside string interpolation plus plus increment depends of the position of plus plus symbols
+    // Before
+    println("My num is ${++myNum}")
+    // After
+    println("My num is ${myNum++}")
+    println(myNum)
+
+    // If Statements
+    var heightPerson1 = 180
+    var heightPerson2 = 180
+
+    if(heightPerson1 > heightPerson2){
+        println("Person 1 is more higher than person 2")
+    }else if(heightPerson1 == heightPerson2){
+        println("Person 2 has the same height than person 1")
+    }
+    else{
+        println("Person 2 is more higher than person 1")
+    }
+
+    println("---------------------")
+
+    var age = 22
+
+    if( age >= 60)
+        println("You're over 60")
+
+    if (age >= 21){
+        println("You may drink")
+    } else if (age >= 18){
+        println("You may vote")
+    } else if (age >= 16){
+        println("You may drive")
+    } else {
+        println("You're too young")
+    }
+
+    var Name = "Carlos"
+
+    if (Name == "Carlos"){
+        println("Welcome Home Carlos")
+    } else{
+        println("Who are you?")
+    }
+
+    val isRainy = true
+    if(isRainy)
+        println("It's rainy")
+
+    println("-------------")
+
+    // Kotlin’s "when" expression is the replacement of the switch statement
+    // from other languages like C, C++, and Java.
+    // It is compact and more powerful than switch statements.
+
+    var season = 3
+
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid season")
+    }
+
+    var month = 4
+
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        //in 12..2 -> println("Autumn")
+        //in 12 downTo 2 -> println("Autumn")
+        12, 1, 2 -> println("Autumn")
+        else -> println("Invalid month")
+    }
+
+    when(age){
+        //in 21..200 -> println("now you may drink in the us")
+        !in 0..20 -> println("now you may drink in the us")
+        in 18..20 -> println("you may vote now")
+        16, 17 -> println("you may drive now")
+        else -> println("You're too young")
+    }
+
+    var x : Any = 13.37f
+
+    when(x){
+        is Int -> println("$x is an Int")
+        is Double -> println("$x is a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }
+
+    val xy : Any = 13.37
+    //assign when to a variable
+    val resultado =  when(x) {
+    //let condition for each block be only a string
+        is Int -> "is an Int"
+        !is Double -> "is not Double"
+        is String -> "is a String"
+        else -> "is none of the above"
+    }
+    //then print x with the result
+    println("$xy $resultado")
+
+    println("-----------------")
+
+    // while loop executes a block of code repeatedly as long as you given condition is true
+    var q = 1
+    while(q <= 10){
+        println("$q")
+        q++
+    }
+    println("\n while loop is done")
+
+    var z = 100
+    while (z >= 0){
+        println("$z")
+        z -= 2
+    }
+
+    println("---------dowhile------------")
+    var y = 1
+
+    do{
+        println("$y")
+        y++
+    }while(y <= 10)
+    println("do while loop is done")
 }
